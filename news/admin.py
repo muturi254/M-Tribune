8885bec65f205d6a4cf410103aa82e5d0beab592
muturi254 <petermuturi74@gmail.com>
2018-05-22 19:36:46 +0300

    adde subclass to admin register for article

--- news/admin.py
+++ news/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Article, Editor, Tags
 
+
 class ArticleAdmin(admin.ModelAdmin):
-    filter_horizontal = ('Tags')
+    filter_horizontal = ('tags',)
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Editor)
 admin.site.register(Tags)
