1c0eb3fc2df8af8926f1609be41325ac7773fcaf
muturi254 <petermuturi74@gmail.com>
2018-05-22 19:07:09 +0300

    edited how tags get displayed in the admin page

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Article, Editor, Tags
 
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Tags')
+
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Editor)
