70582a37818800406070ad85912cf59ff66859ad
muturi254 <petermuturi74@gmail.com>
2018-05-22 16:18:19 +0300

    updated articles with date field for publication

--- news/models.py
+++ news/models.py
@@ -23,4 +23,5 @@ class Article(models.Model):
     title = models.CharField(max_length=60)
     post = models.TextField()
     editor = models.ForeignKey(Editor)
-    tags = models.ManyToManyField(tags)
+    tags = models.ManyToManyField(Tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
