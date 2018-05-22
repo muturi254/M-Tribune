dc2df72a2e87173ec8f481d7bd4cd2d783e6a57a
muturi254 <petermuturi74@gmail.com>
2018-05-22 18:46:37 +0300

    registred models to the admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, Editor, Tags
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Editor)
+admin.site.register(Tags)
