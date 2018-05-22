97ba3c1aeb5b1c357f0407307459576d0e6ee197
muturi254 <petermuturi74@gmail.com>
2018-05-22 19:05:29 +0300

    added string return i Articles

--- news/models.py
+++ news/models.py
@@ -29,3 +29,6 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(Tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
