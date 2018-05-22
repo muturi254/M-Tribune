be86bc02361d6f0df83bb0bd569a8a59858f9ee5
muturi254 <petermuturi74@gmail.com>
2018-05-22 19:36:15 +0300

    test for article creation and calling current date article

--- news/tests.py
+++ news/tests.py
@@ -14,3 +14,27 @@ class EditorTestClas(TestCase):
         self.james.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors)> 0)
+
+class ArticleTestClass(TestCase):
+    def setUp(self):
+        self.james = Editor(first_name='James', last_name='Muriuki', email='james@moringaschool.com')
+        self.save_editor()
+
+        self.new_tag = Tags(name='hello')
+        self.new_tag.save()
+
+        self.new_article = Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        slf.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+        def tearDown(self):
+            Editor.objects.all().delete()
+            Article.objects.all().delete()
+            Tags.objects.all().delete()
+
+        def test_get_news_today(self):
+            todays_news = Article.todays_news()
+            self.assertTrue(len(todays_news)> 0)
+
+        
