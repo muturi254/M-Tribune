bfdc92dd9e5834afffb9ac932335601d4cdf0ca7
muturi254 <petermuturi74@gmail.com>
2018-05-22 23:53:11 +0300

    added search results to urs

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,7 @@ from . import views
 # write zyou views here
 urlpatterns=[
     # url(r'^$', views.welcome, name='welcome'),
-    url(r'^%', views.news_of_day, name='newsToday'),
+    url(r'^$', views.news_of_day, name='newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews'),
+    url(r'^search/$', views.search_results, name='search_results'),
 ]
