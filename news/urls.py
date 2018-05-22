8209f60603f8392fd8437c1f51c93037833ef959
muturi254 <petermuturi74@gmail.com>
2018-05-22 18:52:03 +0300

    changed root iindex path to todays news

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 # write zyou views here
 urlpatterns=[
-    url(r'^$', views.welcome, name='welcome'),
-    url(r'^today/$', views.news_of_day, name='newsToday'),
+    # url(r'^$', views.welcome, name='welcome'),
+    url(r'^%', views.news_of_day, name='newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews'),
 ]
